b5cd8bdf71ae897679eb8fb3c3ec327f41b96485
Pasqualesss <pascale.radu20@gmail.com>
2021-03-02 11:39:33 +0200

    Terminat problema 4

---
+++ Lab1/p4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int palindrom(char *s, int start, int end);
+
+int main(){
+    char s[]="motor";
+    int start = 0;
+    int end = strlen(s)-1;
+    if(palindrom(s, start, end) == 0){
+        printf("Este palindrom\n");
+    }
+    else{
+        printf("Nu este palindrom\n");
+    }
+}
+
+int palindrom(char *s, int start, int end){
+    if(start == end){
+        return 0;
+    }
+    else if(start == end-1){
+        return 0;
+    }
+    if(s[start] == s[end]){
+        return palindrom(s, start+1, end-1);
+    }
+    else{
+        return -1;
+    }
+}
